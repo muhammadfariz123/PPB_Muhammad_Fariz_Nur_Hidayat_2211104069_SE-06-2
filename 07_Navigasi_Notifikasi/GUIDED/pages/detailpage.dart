f30912d8e60aa3d9dcce940198664ffd40d5bf28
muhammadfariz123 <131078732+muhammadfariz123@users.noreply.github.com>
2024-10-30 18:42:20 +0700

    Add files via upload

---
+++ 07_Navigasi_Notifikasi/GUIDED/pages/detailpage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key, required this.data});
+
+  final Widget data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail Page'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[400],
+      ),
+      body: Center(
+        child: data,
+      ),
+    );
+  }
+}
